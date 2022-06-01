648e443701466170d1bae4045999c485c2a7d6d6
zion livingstone <zionlivingstone4@gmail.com>
2022-06-01 15:37:23 +0100

    uncompleted

--- projecteight/app.js
+++ projecteight/app.js
@@ -1,18 +1,26 @@
 const smallbottle=document.querySelectorAll(".smallbottle")
 const smallbottles=document.querySelector(".smallbottles")
+const percentage=document.querySelector(".percentage")
 
 
+function work(x) {
+ smallbottle.forEach((small,index2)=>{
+    if (x>=index2) {
+        small.classList.add("full")
+    }else{small.classList.remove("full")}
+
+ })
+  
+}
 smallbottle.forEach((small,index)=>{
     
-    small.addEventListener("click",(e)=>{
-        e.currentTarget.classList.toggle("full")
-        const x=smallbottles.children
-        x.forEach((work)=>{
-            if (work.classList.contains("full")) {
-                console.log("g");
-            }
-        })
-      
-        
+    small.addEventListener("click",()=>{
+       work(index)
+       console.log(index);
+       percentage.style.height=`${(index+1)*20}%`
     })
+   
+    
 })
+
+
