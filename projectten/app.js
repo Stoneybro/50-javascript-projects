dc3d0227e658ca7f93cf73c999411d213c21b418
zion livingstone <zionlivingstone4@gmail.com>
2022-06-03 16:56:11 +0100

    delete key

---
+++ projectten/app.js
@@ -0,0 +1,32 @@
+const numbers=[1,2,3,"*",4,5,6,"/",7,8,9,".",0,"+","-"]
+const keys=document.querySelectorAll(".keys")
+const question=document.querySelector(".question")
+const equals=document.querySelector("#equals")
+const deleteno=document.querySelector("#delete")
+const clear=document.querySelector("#clear")
+keys.forEach((key,index)=>{
+ 
+    question.textContent;
+    key.addEventListener("click",(e)=>{
+      
+        let x=e.currentTarget
+        for (let i = 0; i < numbers.length; i++) {
+            key= numbers[index];
+        }
+        question.textContent+=key
+       
+    })
+    clear.addEventListener("click",()=>{
+         question.textContent=""
+       
+     })
+     deleteno.addEventListener("click",()=>{
+
+     })
+
+   equals.addEventListener("click",()=>{
+      question.textContent= eval(question.textContent)
+   })
+    
+})
+
